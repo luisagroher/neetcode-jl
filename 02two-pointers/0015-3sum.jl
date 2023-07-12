1c991049d6c6ead8d02178fcdc5172bbf053fd1d
luisa groher <luisagroher@pop-os.localdomain>
2023-07-12 10:15:38 -0400

    modified to be more idiomatically julia and less pythonic in style

--- 02two-pointers/0015-3sum.jl
+++ 02two-pointers/0015-3sum.jl
@@ -1,33 +1,40 @@
 using Test
 
+function ThreeSum(nums::AbstractVector)
+    if eltype(nums) <: Number
+        @error "nums must be a vector of integers"
+    end
+    triplets = Vector{Vector{eltype(nums)}}()
+    return triplets
+end
+
 function ThreeSum(nums::Vector{Int})::Vector{Vector{Int}}
-    nums = sort(nums)
-    result = []
-    for index in 1:length(nums)
-        if nums[index] > 0
-            break
-        end
-        if index > 1 && nums[index] == nums[index - 1]
-            continue
-        end
-        left, right = index + 1, length(nums)
-        while left < right
-            if nums[left] + nums[right] < 0 - nums[index]
+    sorted_nums = sort(copy(nums))
+    n, result, pre = length(nums), Vector{Vector{Int}}(), 0
+    for (i, first) in enumerate(sorted_nums)
+        first == pre && continue
+        pre, left, right = first, i + 1, n
+        @inbounds while left < right
+            total = first + sorted_nums[left] + sorted_nums[right]
+            if total < 0
                 left += 1
-            elseif nums[left] + nums[right] > 0 - nums[index]
+            elseif total > 0
                 right -= 1
             else
-                push!(result, [nums[index], nums[left], nums[right]])
-                left += 1
-                right -= 1
-                while nums[left] == nums[left - 1] && left < right
-                    left += 1
-                end
+                push!(result, [first, sorted_nums[left], sorted_nums[right]])
+                pos = findfirst(x -> x != sorted_nums[left], @view(sorted_nums[left+1:end]))
+                isnothing(pos) ? break : left += pos
+                pos = findfirst(x -> x != sorted_nums[right], @view(sorted_nums[1:right-1]))
+                isnothing(pos) ? break : right -= pos
+
             end
         end
     end
     return result
 end
+
+
+
 
 @test ThreeSum([-1, 0, 1, 2, -1, -4]) == [[-1, -1, 2], [-1, 0, 1]]
 @test ThreeSum([-1, 0, 1, 2, -1, -4, 0, 0, 0, 0]) == [[-1, -1, 2], [-1, 0, 1], [0, 0, 0]]
