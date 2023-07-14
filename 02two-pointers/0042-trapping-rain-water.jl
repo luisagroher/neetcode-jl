f90d2db495ea29050e47e46d6fa10756e67888ed
luisa groher <luisagroher@pop-os.localdomain>
2023-07-13 22:25:40 -0400

    modified trap rain water to experiment with a multiple dispatch solution

--- 02two-pointers/0042-trapping-rain-water.jl
+++ 02two-pointers/0042-trapping-rain-water.jl
@@ -1,22 +1,40 @@
 using Test
 
-function trap(height::Vector{Int})::Int
-    l, r = 1, length(height)
-    leftMax, rightMax = height[l], height[r]
-    res = 0
-    while l < r
-        if leftMax < rightMax
-            l += 1
-            leftMax = max(leftMax, height[l])
-            res += leftMax - height[l]
-        else
-            r -= 1
-            rightMax = max(rightMax, height[r])
-            res += rightMax - height[r]
-        end
-    end
-    return res
+
+function trap(height::Vector{Int})
+  left = 1
+  right = length(height)
+
+  return trap(height, left, right)
 end
+
+function trap(height::Vector{Int}, left::Int, right::Int)
+  result = 0
+
+  while left < right
+    if height[left] < height[right]
+      result += process_left(height, left, right)
+      left += 1
+    else
+      result += process_right(height, left, right)
+      right -= 1
+    end
+  end
+
+  return result
+end
+
+function process_left(height, left, right)
+  left_max = maximum(height[1:left])
+  return left_max - height[left]
+end
+
+function process_right(height, left, right)
+  right_max = maximum(height[right:end])
+  return right_max - height[right]
+end
+
+
 
 @test trap([0,1,0,2,1,0,1,3,2,1,2,1]) == 6
 @test trap([4,2,0,3,2,5]) == 9
